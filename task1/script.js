5d716331f69b3858894d7a8a0f6aaa01cb856a2f
Vovka95 <volodimir.protskiy@gmail.com>
2018-07-28 12:53:38 +0300

    change method 'map'

--- task1/script.js
+++ task1/script.js
@@ -1,29 +1,39 @@
+if(!Array.prototype.map) {
 
-
-if (!Array.prototype.map2) {
-	Array.prototype.map2 = function(func, array) {
+	Array.prototype.map = function(callback, array) {
 	
+  	if (this == null) {
+      throw new TypeError(' this is null or not defined');
+    }
+    
+ 		if(!Array.isArray(this)) {
+    	throw new TypeError(`${this} is not array`); 
+		}
+   
+		if (typeof callback !== 'function') {
+    	throw new TypeError(`${callback} is not a function`);
+    }
+    
 		const newArray =[];
 
-		for (let i = 0; i < array.length; i++) {
-		
-			newArray.push(func(array[i], i, array));
-	
-		}
+		for (let i = 0; i < this.length; i++) {
+    	newArray.push(callback(this[i], i, this))
+    }
 
 		return newArray;
 	}
-
+  
 }
 
-let arr = [1, 4, 9];
-let newArr = arr.map2(
+
+
+let arr = [1,3,5];
+let newArr = arr.map(
 	function(el) {
-		return el ** 2;
+		return el + 2;
 	}
 )
 
 console.log(newArr);
-//console.log(roots2);
 
 
