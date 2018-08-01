23f8023ea1e9b836936a3642c911a4369ecbe0a6
Vovka95 <volodimir.protskiy@gmail.com>
2018-08-01 09:16:34 +0300

    add function decrement2

--- task5/index.js
+++ task5/index.js
@@ -1,10 +1,24 @@
-let arr=[];
-let decrement = (n) => {
+/*let arr=[];
+let decrement1 = (n) => {
     if(n >= 0) {
         arr.push(n);
-        decrement(n-1);
+       return decrement(n-1);
    }else {
     console.log(arr.join(','));
 	}
 }
-decrement(10);
+decrement1(10);*/
+
+
+
+const decrement2 = (a) => {
+	let arr = [];	
+	for (let i = a; i >= 0; i-- ) {
+		
+		arr.push(i);		
+			
+	}
+	console.log(arr.join(', '));
+};
+
+decrement2(10);
